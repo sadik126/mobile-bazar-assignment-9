f2561722a36f219f323c26558c5ac1005701df1f
sadik126 <sadikhimel04@gmail.com>
2022-04-04 13:08:17 +0600

    ms

--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Usereview from '../../Hooks/Usereview';
 import Header from '../Header/Header';
+import Reviewcard from '../Reviewcard/Reviewcard'
 
 const Review = () => {
+    const [reviewer, setReviewer] = Usereview();
     return (
         <div>
             <Header></Header>
-            <h1>this is Review</h1>
+            <h1 className='text-center text-4xl'>this is Review</h1>
+            <div>
+                {
+                    reviewer.map(review => <Reviewcard key={review.id}></Reviewcard>)
+                }
+            </div>
         </div>
     );
 };
--- src/Hooks/Usereview.js
+++ src/Hooks/Usereview.js
@@ -3,8 +3,12 @@ import { useEffect, useState } from "react";
 const Usereview = () => {
     const [reviewer, setReviewer] = useState([]);
     useEffect(() => {
-
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setReviewer(data))
     }, [])
+
+    return [reviewer, setReviewer]
 
 }
 
