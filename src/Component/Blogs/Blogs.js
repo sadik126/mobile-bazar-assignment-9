1109a2606d6400b9875a69f44fc64e521773775d
sadik126 <sadikhimel04@gmail.com>
2022-04-04 20:23:51 +0600

    simmantic tag

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -5,7 +5,11 @@ const Blogs = () => {
     return (
         <div>
             <Header></Header>
-            <p>this is blogs</p>
+
+            <h1 className='text-center text-5xl text-green-600 font-bold'> what is semantic tag?</h1>
+            <p className='text-2xl'>Answer: semantic tag explains the meaning to both browser and developers.By using semantic tag a browser can understand how the website should be look like.Its easy to handle for a browser and developers also.It helps developers to maintain the code easily.Developers can easily handle their website when it needs to update anything.
+                The example of semantic tag: form , table, article ,nav
+                if any developer makes a article by using nav , it hard to understand code for browser and other developers also .Thats why we need semantic tag for get best result</p>
         </div>
     );
 };
